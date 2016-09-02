601fcb31b76e437ef057fa094f4b2438189d94cf
Victor Demin <mail@vdemin.com>
2016-09-02 12:05:57 +0400

    add sqlite database

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'sqlite:@app/data/data.db',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
